c6d75299b7c780b37817ea2322d73334b5062329
cotto89 <cotto89@gmail.com>
2016-01-21 23:24:11 +0900

    database.jsをwrapper classとして定義してみた。

--- src/bg/database.js
+++ src/bg/database.js
@@ -16,12 +16,42 @@
 * ------------------------------------*/
 
 import Dexie from 'dexie'
-export default function () {
-  let db = new Dexie('SashikomiDB');
-  db.version(1).stores({
-    memos: "++id, url"
-  });
-  db.open();
 
-  return db;
+class DB {
+  constructor() {
+    this.db = {};
+    this.setup();
+  }
+
+  setup() {
+    let db = new Dexie('SashikomiDB');
+    db.version(1).stores({ memos: "++id, url" });
+    db.open();
+
+    this.db = db
+  }
+
+  putMemo(obj) {
+    return this.db.transaction('rw', this.db.memos, () => {
+      return this.db.memos.put(obj)
+        .then(id => {
+          return this.db.memos.get(id);
+        })
+    })
+  }
+
+  deleteMemo(id) {
+    return this.db.transaction('rw', this.db.memos, () => {
+      return this.db.memos.delete(id)
+    })
+  }
+
+  getMemosByUrl(url) {
+    return this.db.transaction('rw', this.db.memos, () => {
+      return this.db.memos.where('url').equals(url).toArray()
+    })
+  }
+
 }
+
+export const db = new DB();

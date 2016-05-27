cc981b83700272c4357fb7ff5b220026017b9cad
cotto89 <cottto89@gmail.com>
2016-05-27 22:53:56 +0900

    [modify]Viewer.jsx
    buttonを持つように戻した

--- src/inject/js/components/Viewer.jsx
+++ src/inject/js/components/Viewer.jsx
@@ -1,11 +1,12 @@
 // TODO: iframe
-
 import React, { Component, PropTypes } from 'react';
 import marked from 'marked';
 import highlightJS from 'highlight.js';
 
 const ViewerPropTypes = {
   markdown: PropTypes.string,
+  onEditButtonClick: PropTypes.func.isRequired,
+  onDeleteButtonClick: PropTypes.func.isRequired,
 };
 
 export default class Viewer extends Component {
@@ -23,6 +24,10 @@ export default class Viewer extends Component {
   render() {
     return (
       <div className="Viewer">
+        <div className="BtnGroup">
+          <button type="button" onClick={this.props.onEditButtonClick.bind(this)}>Edit</button>
+          <button type="button" onClick={this.props.onDeleteButtonClick.bind(this)}>Delete</button>
+        </div>
         <div
           className="Viewer__body"
           dangerouslySetInnerHTML={this.rawMarkup() }>

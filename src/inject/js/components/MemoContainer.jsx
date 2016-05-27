68b01b337007cbdbbfe2b82d18d7a0f8ab70e7e2
cotto89 <cottto89@gmail.com>
2016-05-27 23:25:43 +0900

    [modify]MemoContainer.jsx

--- src/inject/js/components/MemoContainer.jsx
+++ src/inject/js/components/MemoContainer.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Base from './Base'
-import Memo from './Memo'
-import Editor from './Editor'
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './Editor.jsx';
+import Viewer from './Viewer.jsx';
 
-export default class MemoContainer extends Base {
+export default class MemoContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,109 +11,84 @@ export default class MemoContainer extends Base {
       url: this.props.url,
       targetElmPath: this.props.targetElmPath,
       contentText: this.props.contentText || "",
-      isEditing: false
+      isEditing: false,
     };
-
-    this._bind(
-      'rendererChild',
-      'handleToggleChild',
-      'handleSubmit',
-      'handleDelete'
-    );
   }
 
   componentWillMount() {
     if (!this.state.contentText.trim()) {
-      this.setState({ isEditing: true })
+      this.setState({ isEditing: true });
     }
   }
 
+  // これは必要あるか？
   componentDidUpdate() {
     if (!this.state.isEditing && !this.state.contentText.trim()) {
       this.removeComponent();
     }
   }
 
-  handleToggleChild() {
-    this.setState({ isEditing: !this.state.isEditing })
+  handleToggleView() {
+    this.setState({ isEditing: !this.state.isEditing });
   }
 
   handleSubmit(text) {
-    let data = Object.assign({}, this.state, { contentText: text });
+    const data = Object.assign({}, this.state, { contentText: text });
     this.setState(data);
 
-    chrome.runtime.sendMessage({ type: 'PUT', data: data },
+    chrome.runtime.sendMessage({ type: 'PUT', data },
       (res) => {
-        if (res.status === 'error') {
-          console.log(res.errorMessage);
-        } else if (res.status === 'success') {
-          this.setState(res.data)
-        }
+        if (res.status === 'success') this.setState(res.data);
       }
     );
   }
 
-
   handleDelete() {
-    let msg = chrome.i18n.getMessage('alert_delete');
+    const msg = chrome.i18n.getMessage('alert_delete');
     if (confirm(msg)) {
-
-      let data = Object.assign({}, this.state);
-
-      chrome.runtime.sendMessage({ type: 'DELETE', data: data },
+      const data = Object.assign({}, this.state);
+      chrome.runtime.sendMessage({ type: 'DELETE', data },
         (res) => {
-          if (res.status === 'error') {
-            console.log(res.errorMessage);
-          } else if (res.status === 'success') {
-            this.removeComponent();
-          }
+          if (res.status === 'success') this.removeComponent();
         }
       );
     }
   }
 
   removeComponent() {
-    let elm = document.getElementById(this.props.containerElmId);
+    const elm = document.getElementById(this.props.containerElmId);
     ReactDOM.unmountComponentAtNode(elm);
     elm.parentNode.removeChild(elm);
   }
 
 
-  rendererChild() {
-    if (this.state.isEditing) {
-      return (
-        <Editor
-          contentText={this.state.contentText}
-          onClose={this.handleToggleChild}
-          onSubmit={this.handleSubmit}
-        />
-      )
-    } else {
-      return (
-        <Memo
-          contentText={this.state.contentText}
-          onClose={this.handleToggleChild}
-          onDelete={this.handleDelete}
-        />
-      )
-    }
-  }
-
   render() {
     return (
-      <div className='chrome__sashikomi'>
-        <div className='chrome__sashikomi--ext'>
-          {this.rendererChild()}
+      <div className="chrome__sashikomi">
+        <div className="chrome__sashikomi--ext">
+          {
+            this.state.isEditing
+              ? <Editor
+                value={this.state.contentText}
+                onCancel={this.handleToggleView.bind(this) }
+                onSubmit={this.handleSubmit.bind(this) }
+                />
+              : <Viewer
+                markdown={this.state.contentText}
+                onEditButtonClick={this.handleToggleView.bind(this) }
+                onDeleteButtonClick={this.handleDelete.bind(this) }
+                />
+          }
         </div>
       </div>
-    )
+    );
   }
 }
 
 MemoContainer.propTypes = {
-  id: React.PropTypes.number,
-  url: React.PropTypes.string.isRequired,
-  contentText: React.PropTypes.string,
-  targetElmPath: React.PropTypes.string.isRequired,
-  containerElmId: React.PropTypes.string.isRequired
+  id: PropTypes.number,
+  url: PropTypes.string.isRequired,
+  contentText: PropTypes.string,
+  targetElmPath: PropTypes.string.isRequired,
+  containerElmId: PropTypes.string.isRequired,
 };
